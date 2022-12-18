df64374ad480bb02613aaec67a4a9567d104d35f
DarXik <davca749@gmail.com>
2022-12-18 21:22:09 +0100

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,77 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
+import React from "react";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import "../src/styles/App.css";
-import Layout from "./pages/layout";
-import Home from "./pages/home";
-import Legal from "./pages/legal";
+import Navbar from "./Navbar";
+import Home from "../src/pages/Home";
+import Story from "../src/pages/Home";
+import Legal from "../src/pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="Legal" element={<Legal />} />
-          {/* <Route path="contact" element={<Contact />} /> */}
-          {/* <Route path="*" element={<NoPage />} /> */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="wrapper">
+      <h1>Marine Mammals</h1>
+      <BrowserRouter>
+        <nav className="w-80 flex justify-around flex-row items-center mx-auto border-2 border-white rounded-2xl shadow-xl">
+          <div>
+            <input type="checkbox" id="overlay-input" />
+            <label htmlFor="overlay-input" id="overlay-button">
+              <span></span>
+            </label>
+            <div id="overlay">
+              <ul>
+                <li><Link to="/">Home</Link> </li>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">Products</a>
+                </li>
+                <li>
+                  <a href="#">Story</a>
+                </li>
+                <li><Link to="/Story">Story</Link></li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+                <li>
+                  <a href="#">Legal</a>
+                </li>
+                <li><Link to="/Legal">Legal</Link></li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <img
+              className="w-28"
+              src="../src/assets/imgs/big-logo.png"
+              alt=""
+            />
+          </div>
+          <div className="flex flex-row items-center gap-x-2 ">
+            <img
+              className="h-auto w-6"
+              src="../src/assets/imgs/shopping-cart.svg"
+              alt=""
+            />
+            <p className="font-semibold text-xs">0</p>
+          </div>
+        </nav>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/Story">
+            <Story />
+          </Route>
+          <Route path="/Legal">
+            <Legal />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom"
 
-export const Navbar = () => {
+export default function Navbar() {
   return (
     <nav className="w-80 flex justify-around flex-row items-center mx-auto border-2 border-white rounded-2xl shadow-xl">
       <div>
@@ -11,24 +10,15 @@ export const Navbar = () => {
         </label>
         <div id="overlay">
           <ul>
-            <li>
-              {/* <a href="#">Home</a> */}
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <a href="#">Products</a>
-            </li>
-            <li>
-              {/* <a href="#">Story</a> */}
-              <Link to="../src/pages/story.jsx"></Link>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-            <li>
-              {/* <a href="../components/privacies.jsx">Legal</a> */}
-              <Link to="../src/pages/legal.jsx"></Link>
-            </li>
+            {/* <li><Link to="/">Home</Link> </li> */}
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Story</a></li>
+            {/* <li><Link to="/Story">Story</Link></li> */}
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Legal</a></li>
+            {/* <li><Link to="/Legal">Legal</Link></li> */}
           </ul>
         </div>
       </div>
@@ -36,9 +26,13 @@ export const Navbar = () => {
         <img className="w-28" src="../src/assets/imgs/big-logo.png" alt="" />
       </div>
       <div className="flex flex-row items-center gap-x-2 ">
-        <img className="h-auto w-6" src="../src/assets/imgs/shopping-cart.svg" alt="" />
+        <img
+          className="h-auto w-6"
+          src="../src/assets/imgs/shopping-cart.svg"
+          alt=""
+        />
         <p className="font-semibold text-xs">0</p>
       </div>
     </nav>
   );
-};
+}
