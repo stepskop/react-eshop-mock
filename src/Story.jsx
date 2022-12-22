32c882a7a8d08788372317d8b50230cc50e96c08
DarXik <davca749@gmail.com>
2022-12-22 12:05:59 +0100

    subpages

--- src/Story.jsx
+++ src/Story.jsx
@@ -3,23 +3,12 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 export default function Story() {
-  return (
-    <>
-      <section>
-        <div className="border-2 border-white rounded-2xl shadow-xl w-80 mx-auto mt-10 p-3 px-4">
-          <h3 className="pb-1.5 text-3xl">About us</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ab
-            quis neque nihil pariatur porro libero obcaecati fugiat architecto
-            maiores. Nesciunt nihil exercitationem voluptas ab dolor! Cum
-            laborum excepturi voluptatem?
-            <br />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ab
-            quis neque nihil pariatur porro libero obcaecati fugiat architecto
-            maiores. Nesciunt nihil exercitat
-          </p>
-        </div>
-      </section>
-    </>
-  );
+    return (
+        <>
+            <section>
+                <div className="border-2 border-white rounded-2xl shadow-xl w-80 mx-auto mt-10 p-3 px-4">
+                </div>
+            </section>
+        </>
+    );
 }
