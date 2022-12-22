494f5bbb3bbe10caf7f7bfef4e84a216d1b30245
DarXik <davca749@gmail.com>
2022-12-22 22:50:42 +0100

    products

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,9 +1,55 @@
 import React from "react";
+import Footer from "./Footer.jsx";
+import {CardProduct} from "./components/card";
+import Carousel from "./components/carousel";
+
 
 export default function Products() {
-  return (
-    <div>
-      <p>Jistě</p>
-    </div>
-  );
+    return (
+        <>
+            <section className="w-80 mx-auto">
+                <div>
+                    <button>a</button>
+                    <button>a</button>
+                    <button>a</button>
+                </div>
+                <div className="mt-4 w-80 mx-auto"  >
+                    <h1 className="text-3xl drop-shadow-xl">
+                       Man T-shirts
+                    </h1>
+                </div>
+                <div className="flex flex-row">
+                    <CardProduct/>
+                    <CardProduct/>
+                </div>
+                <div>
+                    <div className="card w-80 mx-auto bg-base-100 shadow-xl">
+                        <div className="card-body">
+                            <h2 className="card-title">Shoes!</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                        </div>
+                        <figure><img src="../src/assets/imgs/hmgoepprod.jpg" alt="Shoes"/></figure>
+                    </div>
+                </div>
+                <div className="flex flex-row">
+                    <CardProduct/>
+                    <CardProduct/>
+                </div>
+                <div>
+                    <div className="card w-80 mx-auto bg-base-100 shadow-xl">
+                        <div className="card-body">
+                            <h2 className="card-title">Shoes!</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                        </div>
+                        <figure><img src="../src/assets/imgs/hmgoepprod.jpg" alt="Shoes"/></figure>
+                    </div>
+                </div>
+                <div className="flex flex-row">
+                    <CardProduct/>
+                    <CardProduct/>
+                </div>
+            </section>
+            <Footer/>
+        </>
+    );
 }
