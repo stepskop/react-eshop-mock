7e18f894d15e52499d3de6a1a3a7d5a45e0dacc5
DarXik <davca749@gmail.com>
2022-12-29 10:21:15 +0100

    products

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,55 +1,82 @@
 import React from "react";
 import Footer from "./Footer.jsx";
-import {CardProduct} from "./components/card";
-import Carousel from "./components/carousel";
-
+import { CardProduct, CardProductMini } from "./components/card";
 
 export default function Products() {
-    return (
-        <>
-            <section className="w-80 mx-auto">
-                <div className="mt-6 flex gap-x-2">
-                    <button className=" rounded-[10px] bg-[#ffb7c1] px-6 py-[2px] hover:bg-[#ca858ec7] hover:ease-in-out hover:duration-200 active:scale-90 active:ease-in-out active:duration-200">Men</button>
-                    <button className=" rounded-[10px] bg-[#ffb7c1] px-6 py-[2px] hover:bg-[#ca858ec7] hover:ease-in-out hover:duration-200 active:scale-90 active:ease-in-out active:duration-200">Men</button>
-                    
-                </div>
-                <div className="mt-6 w-80 mx-auto"  >
-                    <h1 className="text-3xl drop-shadow-xl">
-                       Man T-shirts
-                    </h1>
-                </div>
-                <div className="flex flex-row">
-                    <CardProduct/>
-                    <CardProduct/>
-                </div>
-                <div>
-                    <div className="card w-80 mx-auto bg-base-100 shadow-xl">
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                        </div>
-                        <figure><img src="../src/assets/imgs/hmgoepprod.jpg" alt="Shoes"/></figure>
-                    </div>
-                </div>
-                <div className="flex flex-row">
-                    <CardProduct/>
-                    <CardProduct/>
-                </div>
-                <div>
-                    <div className="card w-80 mx-auto bg-base-100 shadow-xl">
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                        </div>
-                        <figure><img src="../src/assets/imgs/hmgoepprod.jpg" alt="Shoes"/></figure>
-                    </div>
-                </div>
-                <div className="flex flex-row">
-                    <CardProduct/>
-                    <CardProduct/>
-                </div>
-            </section>
-            <Footer/>
-        </>
-    );
+  return (
+    <>
+      <section className="w-80 mx-auto border border-black">
+        <div className="mt-6 flex gap-x-2">
+          <button className=" rounded-[10px] bg-[#ffb7c1] px-6 py-[2px] hover:bg-[#ca858ec7] hover:ease-in-out hover:duration-200 active:scale-90 active:ease-in-out active:duration-200">
+            Men
+          </button>
+          <button className=" rounded-[10px] bg-[#ffb7c1] px-6 py-[2px] hover:bg-[#ca858ec7] hover:ease-in-out hover:duration-200 active:scale-90 active:ease-in-out active:duration-200">
+            Men
+          </button>
+        </div>
+        <div className="mt-6 w-80 mx-auto">
+          <h1 className="text-3xl drop-shadow-xl">Man T-shirts</h1>
+        </div>
+        <div className="flex flex-row">
+          <CardProductMini
+            image="../src/assets/imgs/hmgoepprod.jpg"
+            title="basic black"
+            desc="you lookin' for something easy and special?"
+          ></CardProductMini>
+          <CardProductMini
+            image="../src/assets/imgs/hmgoepprod.jpg"
+            title="basic black"
+            desc="you lookin' for something easy and special?"
+          ></CardProductMini>
+        </div>
+        <div>
+          <div className="card w-80 mt-10 mx-auto bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+            <figure>
+              <img src="../src/assets/imgs/hmgoepprod.jpg" alt="Shoes" />
+            </figure>
+          </div>
+        </div>
+        <div className="flex flex-row">
+          <CardProductMini
+            image="../src/assets/imgs/hmgoepprod.jpg"
+            title="basic black"
+            desc="you lookin' for something easy and special?"
+          ></CardProductMini>
+          <CardProductMini
+            image="../src/assets/imgs/hmgoepprod.jpg"
+            title="basic black"
+            desc="you lookin' for something easy and special?"
+          ></CardProductMini>
+        </div>
+        <div>
+          <div className="card w-80 mx-auto bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+            <figure>
+              <img src="../src/assets/imgs/hmgoepprod.jpg" alt="Shoes" />
+            </figure>
+          </div>
+        </div>
+        <div className="flex flex-row">
+          <CardProductMini
+            image="../src/assets/imgs/hmgoepprod.jpg"
+            title="basic black"
+            desc="you lookin' for something easy and special?"
+          ></CardProductMini>
+          <CardProductMini
+            image="../src/assets/imgs/hmgoepprod.jpg"
+            title="basic black"
+            desc="you lookin' for something easy and special?"
+          ></CardProductMini>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -16,3 +16,20 @@ export const CardProduct = (props) => {
     </div>
   );
 };
+
+export const CardProductMini = (props) => {
+  return (
+    <div className="w-64 mx-1 mt-10 card card-compact bg-base-100 shadow-xl">
+      <figure>
+        <img src={props.image} />
+      </figure>
+      <div className="inline p-[8px] card-body">
+        <h2 className="mb-2 gap-1 card-title">{props.title}</h2>
+        <p className="pb-2">{props.desc}</p>
+        <div className="card-actions justify-center">
+          <button className="min-h-[2rem] text-[0.8rem] h-8 font-medium btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
