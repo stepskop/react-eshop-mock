132ed4e6fe8b35ca31e20b36da772e57dfab75df
DarXik <davca749@gmail.com>
2022-12-29 17:03:17 +0100

    edits

--- src/Story.jsx
+++ src/Story.jsx
@@ -5,17 +5,16 @@ export default function Story() {
   return (
     <>
       <section className="flex flex-col align-items-center w-80 mx-auto">
-        <h1 className="text-3xl mt-6 px-4">This is we</h1>
-        <div className="border-2 border-white rounded-2xl shadow-xl w-80 mx-auto  p-3 px-4">
-          
+        <div className="border-2 border-white rounded-2xl shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] w-80 mx-auto mt-12 p-3 px-4">
+          <h1 className="text-3xl  pb-4">This is we</h1>
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            <span className="text-[#78aeff] text-xl ">L</span>orem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
             sint dolor reprehenderit neque, quaerat ea in laborum assumenda aut,
             quas cum et inventore. Velit similique soluta delectus fuga
             molestiae fugiat?
           </p>
         </div>
-        <div className="border-2 border-white rounded-2xl shadow-xl w-80 mx-auto mt-10 p-3 px-4">
+        <div className="border-2 border-white rounded-2xl shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] w-80 mx-auto mt-12 p-3 px-4">
           <h2 className="text-center text-xl pb-2">Me</h2>
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
@@ -24,7 +23,7 @@ export default function Story() {
             molestiae fugiat?
           </p>
         </div>
-        <div className="border-2 border-white rounded-2xl shadow-xl w-80 mx-auto mt-10 p-3 px-4">
+        <div className="border-2 border-white rounded-2xl shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] w-80 mx-auto mt-12 p-3 px-4">
           <h2 className="text-xl pb-3">And Him</h2>
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
