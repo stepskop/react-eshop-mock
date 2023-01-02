464921fba7cd3634c4aa79791703b73074177800
StepSkop <74026500+StepSkop@users.noreply.github.com>
2023-01-02 12:30:45 +0100

    Fixed 'navbar burger menu'

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 // import "../styles/App.css";
 
 export default function Navbar() {
+  
   return (
     <nav className="w-80 flex justify-around flex-row items-center mx-auto border-2 border-white rounded-2xl shadow-xl">
       <div>
-        <input type="checkbox" id="overlay-input" />
+        <input type="checkbox" id="overlay-input" defaultChecked={false}/>
         <label htmlFor="overlay-input" id="overlay-button">
           <span></span>
         </label>
@@ -31,7 +32,7 @@ export default function Navbar() {
         </div>
       </div>
       <div>
-        <button><Link href="/"><img className="w-28" src="/imgs/big-logo.png" alt="" /></Link></button>        
+        <button><Link href="/"><img className="w-28" src="/imgs/big-logo.png" alt="" /></Link></button>
       </div>
       <div className="flex flex-row items-center gap-x-2 ">
         <img
